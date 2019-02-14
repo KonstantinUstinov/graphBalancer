efca3afbded4a9d209db6779743f881e82c401e2
kustinov <12345678>
2019-02-14 11:59:24 +0200

    balance main List

--- src/main/scala/com/balancer/data/Node.scala
+++ src/main/scala/com/balancer/data/Node.scala
@@ -6,7 +6,15 @@ case class Node (children: List[Node], leaves: List[Int]) {
 
   def balance(w: Int) : Node = {
 
-    Node(Nil, Nil)
+    leaves.splitByWeight(w, children.length + 1) match {
+      case h::t =>
+        val ch = children.zipAll(t, Nil, Nil)
+          .map(item => Node(item._1.asInstanceOf[Node].children, item._1.asInstanceOf[Node].leaves ::: item._2))
+        Node(ch.map(_.balance(w)), h)
+      case xs =>
+        Node(children, xs.flatten)
+    }
+
   }
 }
 
--- src/test/scala/com/balancer/data/NodeTest.scala
+++ src/test/scala/com/balancer/data/NodeTest.scala
@@ -9,4 +9,15 @@ class NodeTest extends FlatSpec with Matchers {
     result should be ( Node(List(Node(Nil, List(3)), Node(Nil, List(4)), Node(Nil, Nil)), List(1, 2)) )
   }
 
+  "Node" should "balance by 7" in {
+    val result = Node(List(Node(Nil, Nil), Node(Nil, Nil), Node(Nil, Nil)), List(1, 2, 3, 4)).balance(7)
+    result should be ( Node(List(Node(Nil, List(4)), Node(Nil, List()), Node(Nil, Nil)), List(1, 2, 3)) )
+  }
+
+  "Node" should "balance by 4" in {
+    val result = Node(List(Node(Nil, Nil), Node(Nil, Nil), Node(List(Node(Nil, Nil)), Nil)), List(1, 2, 3, 4, 1, 2, 3, 4)).balance(3)
+    result should be ( Node(List(Node(Nil, List(3)), Node(Nil, List(4)), Node(List(Node(Nil, List(3, 4))), List(1, 2))), List(1, 2)) )
+  }
+
+
 }
